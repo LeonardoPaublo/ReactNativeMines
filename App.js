4bd1451a78e4b79b5c98772aed43142124f4ff45
Leonardo Paublo de Oliveira <leonardopaublo@gmail.com>
2021-10-23 02:28:40 -0300

    Criado projeto

---
+++ App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import params from './src/Params';
+import Field from './src/components/Field';
+
+export default class App extends Component {
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.welcome}>Iniciando o Mines!</Text>
+        <Text style={styles.instructions}>Tamanho da grade: {params.getRowsAmount()}x{params.getColumnsAmount()}</Text>
+
+        <Field />
+        <Field opened/>
+        <Field opened nearMines={1} />
+        <Field opened nearMines={2} />
+        <Field opened nearMines={3} />
+        <Field opened nearMines={6} />
+        <Field mined />
+        <Field opened mined />
+        <Field opened mined exploded />
+        <Field flagged />
+      </SafeAreaView>
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+  }
+});
